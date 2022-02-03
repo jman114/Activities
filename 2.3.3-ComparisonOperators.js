3aa74b0dbbc3822012c729149f9592839f0a8777
James McMinn <jmcminn@matchboxdesigngroup.com>
2022-02-02 21:51:02 -0600

    class examples

---
+++ 2.3.3-ComparisonOperators.js
@@ -0,0 +1,33 @@
+// ? log only the expressions that evaluate to true
+console.log(14 > 8);
+console.log(42 > 44);
+console.log(1 > 1);
+
+console.log(31 < 22);
+console.log(9 < 13);
+0 < 0;
+
+12 >= 12.5;
+console.log(60 >= 40);
+console.log(2 >= 2);
+
+console.log(12 <= 12.5);
+14 <= 0.3;
+console.log(5 <= 5);
+
+console.log(3 === 3);
+console.log(3 === "3");
+console.log("3" === "3");
+console.log(3 === 4);
+
+console.log(2 !== "2");
+console.log("2" !== "2");
+2 !== 1;
+
+console.log("two" == 2);
+console.log(2 == "2");
+console.log(2 == 2);
+
+console.log(3 != "three");
+console.log("3" != 3);
+console.log(3 != 3);
---
+++ 2.3.6-TruthyFalsyAndLogicalOperators.js
@@ -0,0 +1,26 @@
+// Logical Operators
+// ? what do you expect each line to print?
+// console.log(true && false); //false
+// console.log(true || false); //true
+// console.log(!true); //false
+// console.log(!(true && false)); //true
+// console.log(false || !false); //true
+
+// Logical Operators and Truthy/Falsy
+// ? what do you expect each line to print?
+// console.log("" && 0); //undefined
+// console.log(undefined || null); //null
+// console.log(!``); //true
+// console.log(!(1 && "false")); //false
+// console.log(!(0 && "false")); //true
+// console.log((1 && "false")); //false
+// console.log((2 && "false")); //false
+// console.log((0 && "false")); //0
+// console.log(NaN || !"true"); //false
+
+// ? TO DO: set the age variable equal to a number, then use the conditional operator to construct an expression that returns "full menu" if the age is over 10, and "kids menu" if the age is less than 10
+let age;
+
+// ? TO DO: using only logical operators (no comparison operators), write an if statement that prints "The number is zero" when num is 0.
+let num = 0;
+// * your if statement here *
