e7c0d9de0c88b8962a0ab38e87f53852e8c7b34c
James McMinn <jmcminn@matchboxdesigngroup.com>
2022-02-02 19:28:15 -0600

    starting 2.3.2

--- 2.3.1-MathOperators.js
+++ 2.3.1-MathOperators.js
@@ -12,12 +12,12 @@
 // console.log("Hello " + "world");
 
 // modulo (remainder)
-console.log(4 % 2);
-console.log(5 % 2);
-console.log(6 % 2);
-console.log(7 % 2);
+console.log(4 % 2); //0
+console.log(5 % 2); //1
+console.log(6 % 2); //0
+console.log(7 % 2); //1
 
-console.log(6 % 3);
-console.log(7 % 3);
-console.log(8 % 3);
-console.log(9 % 3);
+console.log(6 % 3); //0
+console.log(7 % 3); //1
+console.log(8 % 3); //2
+console.log(9 % 3); //0
