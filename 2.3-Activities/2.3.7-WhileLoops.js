1afc7cc3fbb99ed089b84b8e9d44eaa1c2376d93
James McMinn <jmcminn@matchboxdesigngroup.com>
2022-02-03 23:07:14 -0600

    testing changes

--- 2.3-Activities/2.3.7-WhileLoops.js
+++ 2.3-Activities/2.3.7-WhileLoops.js
@@ -1,4 +1,4 @@
-gi// ? TO DO: create a while loop that TOTALS the numbers from 1 to 100 by following these steps...
+// ? TO DO: create a while loop that TOTALS the numbers from 1 to 100 by following these steps...
 // 1) create a variable to represent the current number
 let num = 100;
 
@@ -14,3 +14,16 @@ while (i <= num) {
 }
 
 
+// create a variable outside of the while loop
+// inside the while loop, we keep track of it and re-assign it until the condition becomes false
+let x = 1;
+
+// while x is less than or equal to 10,
+while (x <= 10) {
+  // print x
+  console.log(x);
+
+  // increment by one and re-assign x
+  // if we forgot this step, x <= 10 always evaluates to true and we create an infinite loop
+  x += 2;
+}
