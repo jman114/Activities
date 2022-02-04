de5f390f4f5488bef715a6edb3d800f22ebb43f6
James McMinn <jmcminn@matchboxdesigngroup.com>
2022-02-03 21:00:03 -0600

    All changes

---
+++ 2.3.7-WhileLoops.js
@@ -0,0 +1,16 @@
+// ? TO DO: create a while loop that TOTALS the numbers from 1 to 100 by following these steps...
+// 1) create a variable to represent the current number
+let num = 100;
+
+// 2) create a variable to represent the current total
+let curTot = 0;
+
+// 3) write a while loop that sums the numbers from 1 to 100
+let i = 1;
+while (i <= num) {
+    curTot += i;
+    console.log(curTot);
+    i++;
+}
+
+
---
+++ 2.3.8-ForLoops.js
@@ -0,0 +1,26 @@
+// ? TO DO: create a for loop that TOTALS the numbers from 1 to 100
+
+const num = 100;
+let curTot = 0;
+
+for (let i = 1; i <= num; i++) {
+    curTot += i;
+console.log(curTot);
+}
+
+
+//   const num = 100;
+//   let curTot = 0;
+
+//   for (let i = 1; i <= num; i++) {
+//       curTot += i;
+// console.log(curTot);
+//   }
+
+
+let temp = 100;
+if (temp >= 90){
+console.log(`The weather today is very Hot`);
+} else {
+console.log(`The weather today is very Cold`);
+}
