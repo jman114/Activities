c990c635b59d3c790f8f230cf28f8aea06a0947f
James McMinn <jmcminn@matchboxdesigngroup.com>
2022-02-07 19:02:14 -0600

    Activities 3

---
+++ 3.1-Activities/3.1.1-FunctionDeclaration.js
@@ -0,0 +1,37 @@
+// TODO: using the function expressions below, refactor them into function declarations
+const greet = function() {
+    console.log("Hello!");
+  };
+
+//Changed to declaration 
+  function greet(){
+      console.log("Hello!");
+  }
+  
+  const threeModTwo = function() {
+    console.log(3 % 2);
+  };
+
+  //Changed to declaration
+  function threeModTwo(){
+      console.log(3 %2);
+  };
+  
+  let age = 18;
+  const checkDrivingAge = function() {
+    if (age >= 16) {
+      console.log(true);
+    } else {
+      console.log(false);
+    }
+  };
+
+  //Changed to declaration
+  let age = 18;
+  function checkDrivingAge(){
+      if (age >= 16) {
+          console.log(true);
+      } else {
+          console.log(false);
+      }
+  };
