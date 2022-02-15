03683d7b63b088997312df8fb98f005cf6ed99ea
James McMinn <jmcminn@matchboxdesigngroup.com>
2022-02-14 22:01:09 -0600

    4.1 Activities

--- 4.1-Activities/4.1.2-NodeRelationships.js
+++ 4.1-Activities/4.1.2-NodeRelationships.js
@@ -1,9 +1,23 @@
 // select the root node
+const root = document.querySelector("html");
+console.log("Root Node ... ", root);
 
 // select the last child of the root node
+console.log("Root Node last child ... ", root.lastElementChild);
 
 // select all the children of the body element
+console.log(
+  "Children of the body ... ",
+  document.querySelector("body").children
+);
 
 // select the next sibling of the h2 node
+console.log(
+  "h2 next sibling ... ",
+  document.querySelector("h2").nextElementSibling
+);
 
 // select the parent element of the h1 node
+console.log("Parent of h1 ... ", document.querySelector("h1").parentElement);
+
+console.log("\n ==============================");
--- 4.1-Activities/4.1.3-ChangingElements.js
+++ 4.1-Activities/4.1.3-ChangingElements.js
@@ -1,11 +1,25 @@
 // print the outer HTML of the navigation bar
+const navBar = document.querySelector(".nav-bar");
+console.log("Nav bar outer html ... ", navBar.outerHTML);
 
 // print the inner HTML of the navigation bar
+console.log("Nav bar inner html ... ", navBar.innerHTML);
 
 // print the text content of the #class-schedule-list element
+const classScheduleList = document.querySelector(
+  "#class-schedule-list"
+).textContent;
+console.log("class-schedule-list ... ", classScheduleList);
 
 // select the classList for the first class-week, then add the class "week-1"
+const firstClassWeek = document.querySelector(".class-week");
+firstClassWeek.classList.add("week-1");
 
 // select the img element and add a src attribute
+document.querySelector("img").src =
+  "https://i2.pickpik.com/photos/774/556/1024/odin-male-australian-shepherd-dog-thumb.jpg";
 
 // change the font color of the h1 element
+document.querySelector("h1").style.color = "blue";
+
+console.log("\n ==============================");
