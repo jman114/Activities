051aa775274c5802d08318e0879525708c0eb0a9
James McMinn <jmcminn@matchboxdesigngroup.com>
2022-02-14 18:30:22 -0600

    Activities 4

--- 3.1-Activities/3.1.4-Parameters.js
+++ 3.1-Activities/3.1.4-Parameters.js
@@ -1,33 +1,33 @@
-// Named Parameters
-// write a function that takes two named parameters:
-// print each named parameter,
-// then return the parameters added together
+// // Named Parameters
+// // write a function that takes two named parameters:
+// // print each named parameter,
+// // then return the parameters added together
 
-function functionWithTwoParams(x, y) {
-    console.log("X = ", x);
-    console.log("Y = ", y);
-    return x + y;
-}
+// function functionWithTwoParams(x, y) {
+//     console.log("X = ", x);
+//     console.log("Y = ", y);
+//     return x + y;
+// }
 
-// invoke the function and pass in two numbers
-functionWithTwoParams(5, 8);
+// // invoke the function and pass in two numbers
+// functionWithTwoParams(5, 8);
 
-// invoke the function and pass in more than two numbers
-functionWithTwoParams(1, 2, 3, 4);
+// // invoke the function and pass in more than two numbers
+// functionWithTwoParams(1, 2, 3, 4);
 
-// invoke the function and pass in only one number
-functionWithTwoParams(9);
+// // invoke the function and pass in only one number
+// functionWithTwoParams(9);
 
-//change the function to set default values for the parameters
+// //change the function to set default values for the parameters
 
-    function functionWithTwoParamsDef(x = 5, y = 10) {
-    console.log("2nd X = ", x);
-    console.log("2nd Y = ", y);
-    return x + y;
-}
+//     function functionWithTwoParamsDef(x = 5, y = 10) {
+//     console.log("2nd X = ", x);
+//     console.log("2nd Y = ", y);
+//     return x + y;
+// }
 
-// again, invoke the function and pass in only one number
-functionWithTwoParamsDef(9);
+// // again, invoke the function and pass in only one number
+// functionWithTwoParamsDef(9);
 
 // Rest Operator
 // add a rest operator to the function's parameters
--- 3.2-Activities/3.2.2-MorePracticeWithObjects.js
+++ 3.2-Activities/3.2.2-MorePracticeWithObjects.js
@@ -1,26 +1,30 @@
 // bracket and dot notation
 const key = "1";
 const obj = {
-  key: console.log("the key is 'key'"),
-  1: console.log("the key is 1")
+  key: "the key is 'key'",
+  1: "the key is 1"
 };
-// what will the following lines print?
-// obj[key]; the key is "key" 
-// obj.key; the key is 1
-obj["key"];
+// // what will the following lines print?
+// console.log(obj[key]);
+// //the key is 1
+// console.log(obj.key);
+// //the key is 'key' 
+// console.log(obj["key"]);
+// //the key is 'key'
 
 // create an object with at least four properties, each with a different data type
+// name one of the four properties "collection" and set its value to an Array or Object
+
 const something = {
     x: "This is x",
     number: 3,
     love: true,
-    // name one of the four properties "collection" and set its value to an Array or Object
     collection: [1, 2, "love"],
 };
 
-// access a value in in "collection" property
-console.log("All you need is",
-something.collection[2]);
+// // access a value in in "collection" property
+// console.log("All you need is",
+// something.collection[2]);
 
 // Nested Arrays and Objects
 const keys = ["key", "1", "method", "favorites", "list"];
@@ -30,17 +34,27 @@ obj.method = function() {
 
 // fill in your favorite movie and color below
 obj["favorites"] = {
-  movie: "Scream",
-  number: 0,
-  color: "Blue"
+  movie: "scream",
+  number: 17,
+  color: "blue"
 };
+
 obj.list = ["a", "b", 3, "d", ["e", 4, 5], { f: 6 }];
 
 // how many lines will the following for...of statement print?
+//5
 // what do you expect to see on each line?
-for (let key in keys) {
+for (let key of keys) {
   console.log(`Key is: ${key} and value is: ${obj[key]}`);
 }
+//Key is: key and value is: the key is 'key'
+// 
+//Refer line 31 - console.log(obj.method());
+
 // use a template literal to print a sentence about your favorite movie and color
+
+console.log(
+  `My favorite movie is ${obj.favorites.movie}, and my favorite color is ${obj.favorites.color}.`
+);
 
 // access the values "b", 4, and 6 from obj.list
